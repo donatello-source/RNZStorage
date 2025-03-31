6e1bc5299769ad5dfd088c4d18f3e3a865bb4a4c
Mateusz Galski <mateuszgalski@gmail.com>
2025-03-31 09:11:24 +0200

    feat(api): Added methods in CategoryController

--- symfony/src/Controller/CategoryController.php
+++ symfony/src/Controller/CategoryController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/category')]
 final class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): JsonResponse
+    #[Route('', name: 'category_all', methods: ['GET'])]
+    public function categoryAll(CategoryRepository $categoryRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CategoryController.php',
-        ]);
+        $categories = $categoryRepository->findAll();
+        return $this->json($categories, 200);
     }
 }
