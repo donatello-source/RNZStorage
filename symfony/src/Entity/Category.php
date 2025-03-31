644a5b3cc16683512ee930d0d8b43d58244c97ec
Mateusz Galski <mateuszgalski@gmail.com>
2025-03-31 08:20:19 +0200

    feat(api): Added new entity - Category

---
+++ symfony/src/Entity/Category.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nazwa = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNazwa(): ?string
+    {
+        return $this->nazwa;
+    }
+
+    public function setNazwa(string $nazwa): static
+    {
+        $this->nazwa = $nazwa;
+
+        return $this;
+    }
+}
