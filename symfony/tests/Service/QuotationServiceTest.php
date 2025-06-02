23b318c95e4ea752c9c461d2db7e958c87592a32
donatello-source <mateuszgalski@gmail.com>
2025-06-02 09:57:28 +0200

    test(api): Updated unit tests for QuotationService

--- symfony/tests/Service/QuotationServiceTest.php
+++ symfony/tests/Service/QuotationServiceTest.php
@@ -3,10 +3,15 @@
 namespace App\Tests\Service;
 
 use App\Entity\Quote;
+use App\Entity\Company;
+use App\Entity\Equipment;
+use App\Entity\QuoteDate;
+use App\Entity\QuoteTable;
+use App\Entity\QuoteTableEquipment;
 use App\Repository\QuoteRepository;
-use App\Repository\QuoteEquipmentRepository;
 use App\Service\QuotationService;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -14,19 +19,16 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class QuotationServiceTest extends TestCase
 {
     private $quoteRepositoryMock;
-    private $quoteEquipmentRepositoryMock;
     private $entityManagerMock;
     private QuotationService $quotationService;
 
     protected function setUp(): void
     {
         $this->quoteRepositoryMock = $this->createMock(QuoteRepository::class);
-        $this->quoteEquipmentRepositoryMock = $this->createMock(QuoteEquipmentRepository::class);
         $this->entityManagerMock = $this->createMock(EntityManagerInterface::class);
 
         $this->quotationService = new QuotationService(
             $this->quoteRepositoryMock,
-            $this->quoteEquipmentRepositoryMock,
             $this->entityManagerMock
         );
     }
@@ -61,49 +63,90 @@ class QuotationServiceTest extends TestCase
         $this->quotationService->getQuoteById(999);
     }
 
+    public function testAddQuoteThrowsExceptionOnMissingCompanyId(): void
+    {
+        $this->expectException(BadRequestHttpException::class);
+        $this->quotationService->addQuote([
+            'projekt' => 'Test',
+            'lokalizacja' => 'Warszawa'
+        ]);
+    }
+
+    public function testAddQuoteThrowsExceptionOnCompanyNotFound(): void
+    {
+        $companyRepo = $this->createMock(\Doctrine\ORM\EntityRepository::class);
+        $companyRepo->method('find')->willReturn(null);
+
+        $this->entityManagerMock->method('getRepository')
+            ->willReturn($companyRepo);
+
+        $this->expectException(BadRequestHttpException::class);
+        $this->quotationService->addQuote([
+            'company_id' => 123,
+            'projekt' => 'Test',
+            'lokalizacja' => 'Warszawa'
+        ]);
+    }
+
     public function testAddQuoteSuccessfullyCreatesQuote(): void
     {
-        $data = [
-            'company' => 1,
-            'status' => 'nowa',
-            'dane_kontaktowe' => 'Jan Nowak',
-            'miejsce' => 'Warszawa',
-            'data_wystawienia' => '2024-05-01',
-            'data_poczatek' => '2024-05-10',
-            'data_koniec' => '2024-05-20',
-        ];
+        $company = new Company();
+        $company->setNazwa('Firma');
+        $company->setNip('1234567890');
 
-        $this->entityManagerMock->expects($this->once())->method('persist');
+        $equipment = new Equipment();
+        $equipment->setName('Sprzęt');
+        $equipment->setPrice(100);
+
+        $companyRepo = $this->createMock(\Doctrine\ORM\EntityRepository::class);
+        $companyRepo->method('find')->willReturn($company);
+
+        $equipmentRepo = $this->createMock(\Doctrine\ORM\EntityRepository::class);
+        $equipmentRepo->method('find')->willReturn($equipment);
+
+        $this->entityManagerMock->method('getRepository')
+            ->willReturnMap([
+                [Company::class, $companyRepo],
+                [Equipment::class, $equipmentRepo],
+            ]);
+
+        $this->entityManagerMock->expects($this->atLeastOnce())->method('persist');
         $this->entityManagerMock->expects($this->once())->method('flush');
+        $this->entityManagerMock->expects($this->once())->method('beginTransaction');
+        $this->entityManagerMock->expects($this->once())->method('commit');
+
+        $data = [
+            'company_id' => 1,
+            'projekt' => 'Test Projekt',
+            'lokalizacja' => 'Warszawa',
+            'rabatCalkowity' => 5,
+            'daty' => [
+                ['type' => 'single', 'value' => '2024-06-01', 'comment' => 'test']
+            ],
+            'tabele' => [
+                [
+                    'kategoria' => 'Sala A',
+                    'rabatTabelki' => 10,
+                    'sprzety' => [
+                        [
+                            'id' => 1,
+                            'ilosc' => 2,
+                            'dni' => 3,
+                            'rabat' => 0,
+                            'showComment' => true
+                        ]
+                    ]
+                ]
+            ]
+        ];
 
         $quote = $this->quotationService->addQuote($data);
 
         $this->assertInstanceOf(Quote::class, $quote);
-        $this->assertEquals(1, $quote->getCompany());
-    }
-
-    public function testAddQuoteThrowsExceptionOnMissingData(): void
-    {
-        $this->expectException(BadRequestHttpException::class);
-
-        $this->quotationService->addQuote([
-            'company' => 1
-        ]);
-    }
-
-    public function testAddQuoteThrowsExceptionOnInvalidDate(): void
-    {
-        $this->expectException(BadRequestHttpException::class);
-
-        $this->quotationService->addQuote([
-            'company' => 1,
-            'status' => 'nowa',
-            'dane_kontaktowe' => 'Jan Nowak',
-            'miejsce' => 'Warszawa',
-            'data_wystawienia' => 'invalid-date',
-            'data_poczatek' => '2024-05-10',
-            'data_koniec' => '2024-05-20',
-        ]);
+        $this->assertEquals($company, $quote->getCompany());
+        $this->assertEquals('Test Projekt', $quote->getProjekt());
+        $this->assertEquals('Warszawa', $quote->getLokalizacja());
+        $this->assertEquals(5, $quote->getGlobalDiscount());
     }
 
     public function testDeleteQuoteRemovesEntity(): void
